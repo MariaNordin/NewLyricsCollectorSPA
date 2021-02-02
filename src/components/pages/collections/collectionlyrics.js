2c7c0ab3682f989835b3fa774ed9a8f8c0814977
Maria Nordin <miamarianordin@gmail.com>
2021-02-02 09:32:40 +0100

    added spotifyLink to saved LyricsView

--- src/components/pages/collections/collectionlyrics.js
+++ src/components/pages/collections/collectionlyrics.js
@@ -4,7 +4,7 @@ import { Container, Card, Button } from 'react-bootstrap';
 export default class CollectionLyrics extends Component {
     constructor() {
         super();
-        this.state = { title: '', artist: '', lyrics: '' }
+        this.state = { title: '', artist: '', lyrics: '', spotifyLink: '' }
     }
     componentDidMount() {
         const thisLyrics = this.props.lyrics.find((item) => 
@@ -13,7 +13,9 @@ export default class CollectionLyrics extends Component {
         this.setState({ 
             title: thisLyrics.lyrics.title,
             artist: thisLyrics.lyrics.artist,
-            lyrics: thisLyrics.lyrics.songLyrics });
+            lyrics: thisLyrics.lyrics.songLyrics,
+            spotifyLink: thisLyrics.lyrics.spotifyLink
+        });
     }
 
     handleClose() {
@@ -26,8 +28,11 @@ export default class CollectionLyrics extends Component {
                 <Card className="m-5">
                     <Card.Body className="mt-3 ml-5 mr-5 mb-3">
                         <Card.Title className="ml-3">{this.state.title}</Card.Title>
-                        <Card.Subtitle className="mb-3 ml-3 mt-1">{this.state.artist}                       
-                        </Card.Subtitle>                      
+                        <Card.Subtitle className="mb-3 ml-3 mt-1">{this.state.artist}
+                        <a id="spotify" href={this.state.spotifyLink} target="_blank" rel="noreferrer">
+                            Listen on Spotify
+                        </a>                       
+                        </Card.Subtitle>                  
                         <Card.Text className="ml-3 mr-3">{this.state.lyrics}</Card.Text>                       
                         <Button className="ml-3 mt-3 pl-4 pr-4" 
                             variant="secondary" onClick={() => this.handleClose()}>Close
